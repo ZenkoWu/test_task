ff92fe1a8e5ae9bd97196957814feda148a71af1
ZenkoWu <anna.zagrebaylo.03@mail.ru>
2023-12-16 22:01:36 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { Main } from "./components/Main/Main"
+import  './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { AboutUser } from "./components/AboutUser/AboutUser"
 
 export const App = () => {
 	return (
-		<div></div>
+		<BrowserRouter> 
+			<div className='content'>
+				<Routes>
+                    <Route path='/' element={<Main/>}/>
+					<Route path='/aboutUser' element={<AboutUser/>}/>
+                </Routes>
+			</div>
+        </BrowserRouter>
 	)
 }
 
