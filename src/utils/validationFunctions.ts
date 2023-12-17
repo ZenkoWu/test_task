9af3b749af50620da3fe05a08b733a1400ab7a22
ZenkoWu <anna.zagrebaylo.03@mail.ru>
2023-12-17 17:42:19 +0300

    Update validationFunctions.ts

--- src/utils/validationFunctions.ts
+++ src/utils/validationFunctions.ts
@@ -5,3 +5,13 @@ export const validateEmail = (email?: string) => {
     const regular = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     return !email || !regular.test(email)
 }
+
+export const validateNickname = (value?: string) => {
+    const regular = /^[a-zA-Zа-яА-Я0-9]+$/
+    return !value || !regular.test(value)
+}
+
+export const validateName = (value?: string) => {
+    const regular = /^[a-zA-Zа-яА-Я]+$/
+    return !value || !regular.test(value)
+}
