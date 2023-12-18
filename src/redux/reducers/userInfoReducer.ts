9880b7436160e98229d3b7adb482d89d86a99033
ZenkoWu <anna.zagrebaylo.03@mail.ru>
2023-12-18 20:13:27 +0300

    Update userInfoReducer.ts

--- src/redux/reducers/userInfoReducer.ts
+++ src/redux/reducers/userInfoReducer.ts
@@ -5,9 +5,21 @@ const initialState = {
     nickname: '',
     name: '', 
     sername: '',
-    advantages: [],
-    radio: 0,
-    checkbox: [],
+    advantages: [
+        {id: 0, advantage: ''},
+        {id: 1, advantage: ''},
+        {id: 2, advantage: ''},
+    ],
+    radio:  [
+        {id: 1, checked: false},
+        {id: 2, checked: false},
+        {id: 3, checked: false},
+    ],
+    checkbox: [
+        {id: 1, checked: false},
+        {id: 2, checked: false},
+        {id: 3, checked: false},
+    ],
     about: '',
     phone: localStorage.getItem('phone') || '',
     email: localStorage.getItem('email') || '',
@@ -29,6 +41,60 @@ export const userInfoReducer = (
             return {
                 ...state,
                [payload.param]: payload.value
+            }
+        }
+        case USER_INFO.DELETE_ADVANTAGE: {
+            const newAdvantages = state.advantages?.filter(el => el.id !== payload)
+            return {
+                ...state,
+                advantages: newAdvantages
+            }
+        }
+        case USER_INFO.ADD_ADVANTAGE: {
+            return {
+                ...state,
+                advantages: [...state.advantages!, { id: state.advantages!.length , advantage: ''}]
+            }
+        }
+        case USER_INFO.CHANGE_ADVANTAGE: {
+            const newAdvantages = state.advantages!.map(el => {
+                if(el.id === payload.id ) {
+                    el.advantage = payload.value
+                }
+                return el
+            })
+            
+            return {
+                ...state,
+                advantages: newAdvantages
+            }
+        }
+        case USER_INFO.CHECK_CHANGE: {
+            const newCheck = state.checkbox!.map(el => {
+                if(el.id === payload ) {
+                    el.checked = !el.checked
+                }
+                return el
+            })
+            
+            return {
+                ...state,
+                checkbox: newCheck
+            }
+        }
+        case USER_INFO.RADIO_CHANGE: {
+            const newRadio = state.radio!.map(el => {
+                if(el.id === payload ) {
+                    el.checked = true
+                }else {
+                    el.checked = false
+                }
+                return el
+            })
+            
+            return {
+                ...state,
+                radio: newRadio
             }
         }
         default: return state;
